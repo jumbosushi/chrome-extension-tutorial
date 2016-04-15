8c46d4f86ac5985bba5080f25fe1dc26c48d8656
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-04-15 06:54:40 -0700

    Add '<all url>' permission to menifest.json

--- popup.js
+++ popup.js
@@ -4,4 +4,4 @@ function runSwitchjs() {
   });
 }
 
-document.getElementById('clickme').addEventListener('click', runSwitchjs);
+document.getElementById('clickme').addEventListener('click', runSwitchjs());
